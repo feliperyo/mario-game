f2b9eca74034e36402c4901d5cf75cac4ef274cd
feliperyo <ryofeliperyo@gmail.com>
2024-06-17 19:00:00 -0300

    button restart funcionando

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const restart = document.querySelector('.restart')
+
 const mario = document.querySelector('.mario')
 const pipe = document.querySelector('.pipe')
+// const cloud = document.querySelector('.cloud')
 
 const jump = () => {
     mario.classList.add('jump')
@@ -11,6 +13,7 @@ const jump = () => {
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft
+    // const cloudPosition = cloud.offsetLeft
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
     if (pipePosition <= 220 && pipePosition > 0 && marioPosition < 150) {
@@ -22,7 +25,16 @@ const loop = setInterval(() => {
         mario.src = './assets/game-over.png'
         mario.style.width = '190px'
         mario.style.marginLeft = '60px'
+
+        // cloud.style.animation = 'none'
+        // cloud.style.left = `${cloudPosition}px`
+
+        clearInterval(loop)
     }
 }, 10)
+
+restart.addEventListener('click', () => {
+    location.reload(true)
+})
 
 document.addEventListener('keypress', jump)
