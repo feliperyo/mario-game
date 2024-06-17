c46006f43dee42b7db796de17e7f4c1fd2527705
feliperyo <ryofeliperyo@gmail.com>
2024-06-17 16:26:15 -0300

    fazendo mario pular

---
+++ script.js
@@ -0,0 +1,12 @@
+const restart = document.querySelector('.restart')
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
+const jump = () => {
+    mario.classList.add('jump')
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500);
+}
+
+document.addEventListener('keypress', jump)
